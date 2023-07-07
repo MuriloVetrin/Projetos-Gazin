2027e67ad17f8792afab1a8fd379e1a71c3254c5
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-07 10:56:14 -0300

    feat: Cadastro + paginação

--- PHP/CRUD_SEM_BOOTSTRAP/cadastrar.php
+++ PHP/CRUD_SEM_BOOTSTRAP/cadastrar.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+ob_start();
 include_once './conexao.php';
 ?>
 
@@ -12,22 +14,39 @@ include_once './conexao.php';
 </head>
 
 <body>
+    <a href="index.php">Lista</a><br>
+
     <h1>CADASTRAR</h1>
 
     <?php
     $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
     if (!empty($dados['CadUsuario'])) {
-        
-        $query_usuario = "INSERT INTO usuarios (nome, email) VALUES (:nome, :email) ";
-        $cad_usuario = $conn->prepare($query_usuario);
-        $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
-        $cad_usuario->bindParam(':email', $dados['email'], PDO::PARAM_STR);
-        $cad_usuario->execute();
-        if($cad_usuario->rowCount()) {
-            echo "<p style='color: green; '>Usuário cadastrado com sucesso!</p>";
-        }else{
-            echo "<p style='color: #f00; '>Falha ao cadastrar usuário!</p>";
+
+        $empty_input = false;
+
+        $dados = array_map('trim', $dados);
+        if (in_array("", $dados)) {
+            $empty_input = true;
+            echo "<p style='color: #f00; '>Necessario preencher todos os campos!</p>";
+        } elseif (!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)) {
+            $empty_input = true;
+            echo "<p style='color: #f00; '>Necessario preencher o campo com email válido!</p>";
+        }
+
+        if (!$empty_input) {
+            $query_usuario = "INSERT INTO usuarios (nome, email) VALUES (:nome, :email) ";
+            $cad_usuario = $conn->prepare($query_usuario);
+            $cad_usuario->bindParam(':nome', $dados['nome'], PDO::PARAM_STR);
+            $cad_usuario->bindParam(':email', $dados['email'], PDO::PARAM_STR);
+            $cad_usuario->execute();
+            if ($cad_usuario->rowCount()) {
+                unset($dados);
+                $_SESSION['msg'] = "<p style='color: green; '>Usuário cadastrado com sucesso!</p>";
+                header("Location: index.php");
+            } else {
+                echo "<p style='color: #f00; '>Falha ao cadastrar usuário!</p>";
+            }
         }
     }
 
@@ -35,10 +54,20 @@ include_once './conexao.php';
 
     <form name="cad-usuario" method="POST" action="">
         <label>Nome: </label>
-        <input type="text" name="nome" id="nome" placeholder="Nome completo" require><br><br>
+        <input type="text" name="nome" id="nome" value=" <?php
+                                                            if (isset($dados['nome'])) {
+                                                                echo $dados['nome'];
+                                                            }
+
+                                                            ?>"><br><br>
 
         <label>Email: </label>
-        <input type="email" name="email" id="email" placeholder="Seu melhor e-mail" require><br><br>
+        <input type="email" name="email" id="email" value=" <?php
+                                                            if (isset($dados['email'])) {
+                                                                echo $dados['email'];
+                                                            }
+
+                                                            ?>"><br><br>
 
         <input type="submit" value="Cadastrar" name="CadUsuario">
     </form>
---
+++ PHP/CRUD_SEM_BOOTSTRAP/index.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+include_once './conexao.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD SIMPLES</title>
+</head>
+
+<body>
+
+    <a href="cadastrar.php">Cadastrar</a><br>
+    <h1>LISTA DE USUARIOS</h1>
+
+    <?php
+
+    if(isset($_SESSION['msg'])) {
+        echo $_SESSION['msg'];
+        unset($_SESSION['msg']);
+    }
+
+    $pagina_atual = filter_input(INPUT_GET, "page", FILTER_SANITIZE_NUMBER_INT);
+    $pagina = (!empty($pagina_atual)) ? $pagina_atual : 1;
+
+    $limite_resultado = 2;
+
+    $inicio = ($limite_resultado * $pagina) - $limite_resultado;
+
+    $query_usuarios = "SELECT id, nome, email FROM usuarios ORDER BY id DESC LIMIT $inicio, $limite_resultado";
+    $result_usuarios = $conn->prepare($query_usuarios);
+    $result_usuarios->execute();
+
+    if (($result_usuarios) and ($result_usuarios->rowCount() != 0)) {
+        while ($row_usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)) {
+
+            extract($row_usuario);
+
+            echo "ID: $id <br>";
+            echo "Nome: $nome <br>";
+            echo "E-mail: $email <br>";
+            echo "<hr>";
+        }
+
+        $query_qnt_registros = "SELECT COUNT(id) AS num_result FROM usuarios";
+        $result_qnt_registros = $conn->prepare($query_qnt_registros);
+        $result_qnt_registros->execute();
+        $row_qnt_registros = $result_qnt_registros->fetch(PDO::FETCH_ASSOC);
+
+        $qnt_pagina = ceil($row_qnt_registros['num_result'] / $limite_resultado);
+
+        $maximo_link = 2;
+        
+        echo "<a href='index.php?page=1'>Primeira</a>";
+
+        for ($pagina_anterior = $pagina - $maximo_link; $pagina_anterior <= $pagina - 1; $pagina_anterior++) {
+            if ($pagina_anterior >= 1) {
+                echo "<a href='index.php?page=$pagina_anterior'>$pagina_anterior</a> ";
+            }
+        }
+
+        echo "$pagina";
+
+        for ($proxima_pagina = $pagina + 1; $proxima_pagina <= $pagina + $maximo_link; $proxima_pagina++) {
+            if($proxima_pagina <= $qnt_pagina) {
+                echo "<a href='index.php?page=$proxima_pagina'>$proxima_pagina</a>";
+            }
+        }
+
+        echo "<a href='index.php?page=$qnt_pagina'>Última</a>";
+    } else {
+        echo "<p style='color: #f00; '>Erro: Nenhum usuário encontrado!</p>";
+    }
+    ?>
+</body>
+
+</html>
