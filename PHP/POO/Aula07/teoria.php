bb4db7057b3afefe327db718fa61ef697e097a87
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-12 10:48:53 -0300

    feat: Aula07 - teoria

---
+++ PHP/POO/Aula07/teoria.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Aula 07 - Rwlacionamento entre classes</title>
+   <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+      <img class="imagem" src="imgs/1.png">
+      <img class="imagem" src="imgs/2.png">
+      <img class="imagem" src="imgs/3.png">
+      <img class="imagem" src="imgs/4.png">
+      <img class="imagem" src="imgs/5.png">
+      <img class="imagem" src="imgs/6.png">
+      <img class="imagem" src="imgs/7.png">
+      <img class="imagem" src="imgs/8.png">
+      <img class="imagem" src="imgs/9.png">
+      <img class="imagem" src="imgs/10.png">
+   </ul>
+</body>
+
+</html>
