bb0b59c225b6d6b78ab49ff91fa237dbaa5d6d0c
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-12 11:58:49 -0300

    feat: Aula07 - pratica - incompleta

---
+++ PHP/POO/Aula07/lutador.php
@@ -0,0 +1,101 @@
+<?php
+class Lutador {
+    //atributos
+    private $nome;
+    private $nacionalidade;
+    private $idade, $altura, $peso;
+    private $categoria, $vitorias, $derrotas, $empates;
+
+    //metodos
+    function apresentar() {
+
+    }
+    function status() {
+        
+    }
+    function ganharLuta() {
+        
+    }
+    function perderLuta() {
+        
+    }
+    function empatarLuta() {
+        
+    }
+    //Métodos Especiais
+    function __construct($no, $na, $id, $al, $pe, $vi, $de, $em)
+    {
+        $this->nome = $no;
+        $this->nacionalidade = $na;
+        $this->idade = $id;
+        $this->altura = $al;
+        $this->peso = $pe;
+        $this->vitorias = $vi;
+        $this->derrotas = $de;
+        $this->empates = $em;
+    }
+
+    function getNome() {
+        return $this->nome;
+    }
+
+    function getNacionalidade() {
+        return $this->nacionalidade;
+    }
+
+    function getIdade() {
+        return $this->idade;
+    }
+
+    function getAltura() {
+        return $this->altura;
+    }
+
+    function getPeso() {
+        return $this->peso;
+    }
+
+    function getVitorias() {
+        return $this->vitorias;
+    }
+
+    function getDerrotas() {
+        return $this->derrotas;
+    }
+
+    function getEmpates() {
+        return $this->empates;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function setNacionalidade($nacionalidade) {
+        $this->nacionalidade = $nacionalidade;
+    }
+
+    function setIdade($idade) {
+        $this->idade = $idade;
+    }
+
+    function setAltura($altura) {
+        $this->altura = $altura;
+    }
+
+    function setPeso($peso) {
+        $this->peso = $peso;
+    }
+
+    function setVitorias($vitorias) {
+        $this->vitorias = $vitorias;
+    }
+
+    function setDerrotas($derrotas) {
+        $this->derrotas = $derrotas;
+    }
+
+    function setEmpates($empates) {
+        $this->empates = $empates;
+    }
+}
