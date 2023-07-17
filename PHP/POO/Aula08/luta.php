27864ba0839ab625a4bca5721a515be71f3b76bb
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-17 09:29:06 -0300

    fix: aula08

--- PHP/POO/Aula08/luta.php
+++ PHP/POO/Aula08/luta.php
@@ -6,14 +6,74 @@ class Luta {
     private $rounds;
     private $aprovada;
 
-    public function marcarLuta() {
-
+    public function marcarLuta($l1, $l2) {
+        if ($l1->getCategoria() === $l2->getCategoria() && ($l1 != $l2)) {
+            $this->aprovada = true;
+            $this->desafiado = $l1;
+            $this->desafiante = $l2;
+        } else {
+            $this->aprovada = false;
+            $this->desafiado = null;
+            $this->desafiante = null;
+        }
     }
     public function lutar() {
-
+        if ($this->aprovada) {
+            $this->desafiado->apresentar();
+            $this->desafiante->apresentar();
+            $vencedor = rand(0,2);
+            switch($vencedor) {
+                case 0: // Empate
+                    echo "<p>Empate!</p>";
+                    $this->desafiado->empatarLuta();
+                    $this->desafiante->empatarLuta();
+                    break;
+                case 1: // Desafiado vence
+                    echo "<p>". $this->desafiado->getNome()."Venceu</p>";
+                    $this->desafiado->ganharLuta();
+                    $this->desafiante->perderLuta();
+                    break;
+                case 2: //Desafiante vence
+                    echo "<p>". $this->desafiado->getNome()."Perdeu</p>";
+                    $this->desafiante->ganharLuta();
+                    $this->desafiado->perderLuta();
+                    break;
+            } 
+        } else {
+            echo "<p>Luta não pode acontecer</p>";
+        }
     }
 
+    //metodos especiais
     function getDesafiado() {
-        return $this->desafiado
+        return $this->desafiado;
+    }
+
+    function getDesafiante() {
+        return $this->desafiante;
+    }
+
+    function getRounds() {
+        return $this->rounds;
+    }
+
+    function getAprovada() {
+        return $this->aprovada;
+    }
+
+    function setDesafiado($desafiado) {
+        $this->desafiado = $desafiado;
+    }
+
+    function setDesafiante($desafiante) {
+        $this->desafiante = $desafiante;
+    }
+
+    function setRounds($rounds) {
+        $this->rounds = $rounds;
+    }
+
+    function setAprovada($aprovada) {
+        $this->aprovada = $aprovada;
     }
 }
--- PHP/POO/Aula08/pratica..php
+++ PHP/POO/Aula08/pratica..php
@@ -11,16 +11,21 @@
   <pre>
     <?php 
     require_once 'Lutador.php';
+    require_once 'Luta.php';
 
-    $N = array();
+    $l = array();
 
-    $N[0] = new Lutador("Pretty Boy", "França", 30, 1.75, 68.9, 11, 2, 1);
-    $N[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
-    $N[2] = new Lutador("SnapShadow", "EUA", 35, 1.70, 84.6, 12, 2, 3);
-    $N[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
-    $N[4] = new Lutador("UFOCobal", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
-    $N[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
+    $l[0] = new Lutador("Pretty Boy", "França", 30, 1.75, 68.9, 11, 2, 1);
+    $l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+    $l[2] = new Lutador("SnapShadow", "EUA", 35, 1.70, 84.6, 12, 2, 3);
+    $l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+    $l[4] = new Lutador("UFOCobal", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+    $l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81, 105.7, 12, 2, 4);
 
+
+    $UFC01 = new Luta();
+    $UFC01->marcarLuta($l[0], $l[1]);
+    $UFC01->lutar();
     ?>
    </pre>
 </body>
