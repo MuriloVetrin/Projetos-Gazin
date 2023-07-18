d4f6444f725b6168ebc37b5b8f17ad5e394e682b
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-18 11:18:47 -0300

    feat: Criando componentes no React

--- REACT/projeto01/src/App.js
+++ REACT/projeto01/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
       <p>Olá, {newName}</p>
       <p>Soma: {sum(1, 2)}</p>
       <img src={url} alt="Minha Imagem" />
+      <HelloWorld />
     </div>
   );
 }
