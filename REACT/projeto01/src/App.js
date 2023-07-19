66d57d20b1cf3a8d18edc18a9e6244bdf57e996a
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-19 11:28:35 -0300

    feat: onClick / onChange / onSubmit

--- REACT/projeto01/src/App.js
+++ REACT/projeto01/src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
+import Evento from './components/Evento'
+import Form from './components/Form';
+/*
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import List from './components/List';
-
+*/
 function App() {
-
-  const nome = 'Murilo'
-
   return (
     <div className="App">
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Form/>
+      {/*
       <HelloWorld />
       <SayMyName nome="Murilo" />
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
       <Pessoa nome="Rodigo" idade="28" profissao="Programador" foto="https://hips.hearstapps.com/hmg-prod/images/2023-chevrolet-corvette-stingray-convertible-3lt-z51-116-1665496966.jpg?crop=0.612xw:0.459xh;0.107xw,0.541xh&resize=1200:*" />
       <List/>
+      */}
     </div>
   );
 }
---
+++ REACT/projeto01/src/components/Evento.js
@@ -0,0 +1,14 @@
+function Evento({ numero }) {
+    function meuEvento() {
+        console.log(`Opa, fui ativado! ${numero}`);
+    }
+
+    return (
+        <div>
+            <p>Clique para disparar um evento:</p>
+            <button onClick={meuEvento}>Ativar</button>
+        </div>
+    );
+}
+
+export default Evento;
