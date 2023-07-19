9bda4a60a14779dee27254b5523b2273eb1636d2
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-19 09:22:05 -0300

    feat: Fragments

--- REACT/projeto01/src/App.js
+++ REACT/projeto01/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import List from './components/List';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
       <Pessoa nome="Rodigo" idade="28" profissao="Programador" foto="https://hips.hearstapps.com/hmg-prod/images/2023-chevrolet-corvette-stingray-convertible-3lt-z51-116-1665496966.jpg?crop=0.612xw:0.459xh;0.107xw,0.541xh&resize=1200:*" />
+      <List/>
     </div>
   );
 }
---
+++ REACT/projeto01/src/components/List.js
@@ -0,0 +1,21 @@
+//import Item from "./item"
+
+function List() {
+    return (
+        <>
+            <h1>Minha Lista</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </>
+    )
+}
+
+/**
+  <Item marca="Ferrari"/>
+  <Item marca="Fiat"/>
+  <Item marca="Renault"/>
+ */
+
+export default List
