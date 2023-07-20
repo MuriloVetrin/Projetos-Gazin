f541a4104bc67dda0d814ebb69f026ae786536ee
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-20 11:57:43 -0300

    feat: Começando a estruturar o projeto

--- REACT/costs/src/App.js
+++ REACT/costs/src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
+import { BrowserRoute as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Route>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <switch>
+        <Route path="/">
+          <Home/>
+        </Route>
+        <Route path="/company">
+          <Company/>
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+        <Route path="/newproject">
+          <NewProject/>
+        </Route>
+      </switch>
+      <p>Footer</p>
+    </Route>
   );
 }
 
