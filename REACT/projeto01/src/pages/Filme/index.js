e0f0d78e9747def22c6af7f51d32b44def8f7c59
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-27 11:42:42 -0300

    feat: Estrutura de rotas

---
+++ REACT/projeto01/src/pages/Filme/index.js
@@ -0,0 +1,9 @@
+function Filme(){
+    return(
+        <div>
+            <h1>Bem Vindo a Pagina detalhes do filme</h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ REACT/projeto01/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            <h1>Bem Vindo a home</h1>
+        </div>
+    )
+}
+
+export default Home;
