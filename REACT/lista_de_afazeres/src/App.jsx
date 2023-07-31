657aded355e25d6eae0e9a08607e616b5fd3885c
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-31 11:19:09 -0300

    feat: Meio de manibular dados do to do

--- REACT/lista_de_afazeres/src/App.jsx
+++ REACT/lista_de_afazeres/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo todo={todo} />
+          <Todo key={todo.id} todo={todo} />
         ))}
       </div>
       <TodoForm />
--- REACT/lista_de_afazeres/src/components/TodoForm.jsx
+++ REACT/lista_de_afazeres/src/components/TodoForm.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import { useState } from 'react'
 
 const TodoForm = () => {
-    return <div className="todo-form">
-        <h2>Criar tarefa:</h2>
-        <form>
-            <input type="text" placeholder="Digite o titulo" />
-            <select>
-                <option value="">Selecione uma categoria</option>
-                <option value="Trabalho">Trabalho</option>
-                <option value="Pessoal">Pessoal</option>
-                <option value="Edtudos">Edtudos</option>
-            </select>
-            <button type="submit">Criar Tarefa</button>
-        </form>
-    </div>
+    const [value, setValue] = useState("");
+    const [category, setCategory] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value || !category) return;
+        // adicionar todo
+        setValue("");
+        setCategory("");
+    };
+
+    return (
+        <div className="todo-form">
+            <h2>Criar tarefa:</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Digite o titulo"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value="">Selecione uma categoria</option>
+                    <option value="Trabalho">Trabalho</option>
+                    <option value="Pessoal">Pessoal</option>
+                    <option value="Estudos">Estudos</option>
+                </select>
+                <button type="submit">Criar Tarefa</button>
+            </form>
+        </div>
+    );
 }
 
-export default TodoForm
+export default TodoForm;
