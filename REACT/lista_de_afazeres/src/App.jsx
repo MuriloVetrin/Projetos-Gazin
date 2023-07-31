837806eec7ddc4e566716f4a67cf8156183c5720
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-07-31 09:59:47 -0300

    fix: add css

--- REACT/lista_de_afazeres/src/App.jsx
+++ REACT/lista_de_afazeres/src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Criar funcionalidade x no sistema',
+      category: 'Trabalho',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: 'Ir para a academia',
+      category: 'Pessoal',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: 'Estudar react',
+      category: 'Estudos',
+      isCompleted: false,
+    },
+  ]);
 
   return (
-    <di>
-      Olá React
-    </di>
-  )
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">{todo.category}</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
