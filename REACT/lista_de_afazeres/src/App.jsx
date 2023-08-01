41326d36cadea1d2a99d29d18ea0ed6d56e1a80b
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-01 09:01:07 -0300

    feat: completar finalizado

--- REACT/lista_de_afazeres/src/App.jsx
+++ REACT/lista_de_afazeres/src/App.jsx
@@ -39,12 +39,33 @@ function App() {
     setTodos(newTodos);
   };
 
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) =>
+      todo.id === id ? todo.isCompleted = !todo.isCompleted : todo
+    );
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} />
+          <Todo
+            key={todo.id}
+            todo={todo}
+            removeTodo={removeTodo}
+            completeTodo={completeTodo}
+          />
         ))}
       </div>
       <TodoForm addTodo={addTodo} />
--- REACT/lista_de_afazeres/src/components/Todo.jsx
+++ REACT/lista_de_afazeres/src/components/Todo.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
     return (
-        <div className="todo" key={todo.id}>
+        <div
+            className="todo"
+            style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+
+        > 
             <div className="content">
                 <p>{todo.text}</p>
                 <p className="category">{todo.category}</p>
             </div>
             <div>
-                <button className="complete">Completar</button>
-                <button className="remove">X</button>
+                <button className="complete" onClick={() => completeTodo(todo.id)}>
+                    Completar
+                </button>
+                <button className="remove" onClick={() => removeTodo(todo.id)}>
+                    X
+                </button>
             </div>
         </div>
     )
