c86facf7185291f53e374c158d7aa28049fa0007
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-02 11:56:39 -0300

    fix: Navbar

--- REACT/lista_de_filmes/src/App.jsx
+++ REACT/lista_de_filmes/src/App.jsx
@@ -1,18 +1,13 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <nav id="navbar">
-        <h2>
-          <Link to="/">Lista de Filmes</Link>
-        </h2>
-        <Link to="/movie/1">Movie</Link>
-        <Link to="/search">Search</Link>
-      </nav>
-      
+      <Navbar />
       <Outlet />
     </div>
   );
