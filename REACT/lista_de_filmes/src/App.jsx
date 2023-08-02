7e7c6077e6d0c62dc851ccd14eabfcd095ac8c4d
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-02 11:32:38 -0300

    feat: Criando rotas

--- REACT/lista_de_filmes/src/App.jsx
+++ REACT/lista_de_filmes/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
-      <h1>Oi</h1>
+      <nav id="navbar">
+        <h2>
+          <Link to="/">Lista de Filmes</Link>
+        </h2>
+        <Link to="/movie/1">Movie</Link>
+        <Link to="/search">Search</Link>
+      </nav>
+      
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
