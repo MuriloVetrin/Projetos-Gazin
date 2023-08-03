a9c57102930664f5d761d254524c07a7b4bce2f9
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-03 10:09:00 -0300

    feat: Card Filmes

--- REACT/lista_de_filmes/src/pages/Home.jsx
+++ REACT/lista_de_filmes/src/pages/Home.jsx
@@ -22,8 +22,13 @@ const Home = () => {
   }, [])
 
   return (
-    <div>
-      {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
+    <div className="container">
+      <h2 className="title">Melhores Filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   );
 };
