b5704e37f86952dc7156f861b55fdcee66e3d8f5
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-03 09:36:58 -0300

    feat: Imprimindo nome dos filmes na home

--- REACT/lista_de_filmes/src/pages/Home.jsx
+++ REACT/lista_de_filmes/src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
-  return <div>Home</div>
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, [])
+
+  return (
+    <div>
+      {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
+    </div>
+  );
 };
 
 export default Home;
