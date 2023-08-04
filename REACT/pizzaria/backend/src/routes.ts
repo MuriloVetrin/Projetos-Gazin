42d28cce6855e7d95e0e08408bdca1d596d13204
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-04 11:58:27 -0300

    feat: Projeto pizza

---
+++ REACT/pizzaria/backend/src/routes.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+    return res.json({ ok: true })
+})
+
+export { router };
