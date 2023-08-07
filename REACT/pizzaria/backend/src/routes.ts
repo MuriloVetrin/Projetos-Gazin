8e9b69a456c801cac51b6f8b6429d9d9b3c7f961
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-07 09:04:27 -0300

    feat: tratamento de erros -> express-async-errors / cors

--- REACT/pizzaria/backend/src/routes.ts
+++ REACT/pizzaria/backend/src/routes.ts
@@ -3,7 +3,8 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ nome: 'Pizzaria' })
+    return res.json({ nome: 'Pizzaria'});
+    //throw new Error('Erro ao fazer esse requisição')
 })
 
 export { router };
