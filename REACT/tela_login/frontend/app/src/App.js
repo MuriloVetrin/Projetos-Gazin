90575897da4dd1e21e1d3ff11607dbea94d7498e
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-14 10:54:21 -0300

    login feitp

--- REACT/tela_login/frontend/app/src/App.js
+++ REACT/tela_login/frontend/app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-  
+      <Login />
       </header>
     </div>
   );
