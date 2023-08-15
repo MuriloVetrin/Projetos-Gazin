636ab2f9191896e5666e734a45b409e645847c3d
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-08-15 08:46:28 -0300

    add msg de erro db conexão

--- REACT/CRUD_REACT_NODE/backend/controllers/users.js
+++ REACT/CRUD_REACT_NODE/backend/controllers/users.js
@@ -4,7 +4,10 @@ export const getUsers = (_, res) => {
   const q = "SELECT * FROM usuarios";
 
   db.query(q, (err, data) => {
-    if (err) return res.json(err);
+    if (err) {
+      console.error("Erro ao buscar usuários:", err.message);
+      return res.status(500).json({ error: "Erro ao buscar usuários" });
+    }
 
     return res.status(200).json(data);
   });
--- REACT/CRUD_REACT_NODE/backend/db.js
+++ REACT/CRUD_REACT_NODE/backend/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "@LB@572",
+    password: "MaCl@891",
     database: "crud"
-})
+});
