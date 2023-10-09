b6a2589a2b89424ae8db3746204bdc4e265df0a8
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-10-09 09:45:48 -0300

    feat: tentar de novo + número de tentativas

--- JAVASCRIPT/logica-js-projeto_inicial/app.js
+++ JAVASCRIPT/logica-js-projeto_inicial/app.js
@@ -1,14 +1,20 @@
 alert("Boas vindas ao jogo do número secreto");
 let numeroSecreto = 7;
 console.log(numeroSecreto);
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute;
+let tentativas =1;
 
-if (chute == numeroSecreto) {
-  alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-  if (chute > numeroSecreto) {
-    alert(`O número secreto é menor que ${chute}`);
+while (chute != numeroSecreto) {
+  chute = prompt("Escolha um número entre 1 e 10");
+
+  if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
   } else {
-    alert(`O número secreto é maior que ${chute}`); 
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+    tentativas++;
   }
 }
