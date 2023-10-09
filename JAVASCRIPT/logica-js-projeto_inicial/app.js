1073b11235ad5eda62a348746c1e9490322c18de
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-10-09 10:33:53 -0300

    feat: Gerador de números aleatórios

--- JAVASCRIPT/logica-js-projeto_inicial/app.js
+++ JAVASCRIPT/logica-js-projeto_inicial/app.js
@@ -1,11 +1,11 @@
 alert("Boas vindas ao jogo do número secreto");
-let numeroSecreto = 7;
+let numeroSecreto = parseInt(Math.random() * 100 + 1) ;
 console.log(numeroSecreto);
 let chute;
 let tentativas =1;
 
 while (chute != numeroSecreto) {
-  chute = prompt("Escolha um número entre 1 e 10");
+  chute = prompt("Escolha um número entre 1 e 100");
 
   if (chute == numeroSecreto) {
     break;
