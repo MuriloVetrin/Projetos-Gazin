3ad65d1f0441c748e4441d14aa3f62e01aa599c7
Murilo Veetorazo <murilo.veetorazo@gazin.com.br>
2023-10-09 10:15:52 -0300

    feat: Operador ternário

--- JAVASCRIPT/logica-js-projeto_inicial/app.js
+++ JAVASCRIPT/logica-js-projeto_inicial/app.js
@@ -19,9 +19,11 @@ while (chute != numeroSecreto) {
   }
 }
 
-if (tentativas > 1) {
-  alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-} else {
-  alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
+//if (tentativas > 1) {
+//  alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+//} else {
+//  alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+//}
